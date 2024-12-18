4ccbef4a756b4da5c2dc207c91eca99066f8842e
FazeElian <elian.ibarra@soy.sena.edu.co>
2024-12-18 13:36:25 -0500

    create: Products nest module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsModule } from './modules/products/products.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ProductsModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
