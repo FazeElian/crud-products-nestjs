365b21f8cb246de5b1690e7560cd9a61a1dcf37a
FazeElian <elian.ibarra@soy.sena.edu.co>
2024-12-18 13:48:22 -0500

    add: Creation and initializing of Products array

--- src/modules/products/products.service.ts
+++ src/modules/products/products.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+    // Products private array
+    private _products: string[];
+
+    constructor() {
+        // Initial value of empty
+        this._products = [];
+    }
+}
