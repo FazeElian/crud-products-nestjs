aac303376a7f50d005a9000b67e236758badef14
FazeElian <elian.ibarra@soy.sena.edu.co>
2024-12-18 13:59:35 -0500

    add: Create product function using Post & Body decorators

--- src/modules/products/products.controller.ts
+++ src/modules/products/products.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ProductsService } from './products.service';
 
 @Controller('api/products')
-export class ProductsController {}
+export class ProductsController {
+    // Linking controller with the service
+    constructor(private ProductsService: ProductsService) {
+
+    }
+
+    // Decorator for the method
+    @Post()
+    createProduct(@Body() data: { product: string }) {
+        // call the method from the service when we send the product
+        return this.ProductsService.createProduct(data.product)
+    }
+}
--- src/modules/products/products.service.ts
+++ src/modules/products/products.service.ts
@@ -9,4 +9,13 @@ export class ProductsService {
         // Initial value of empty
         this._products = [];
     }
+
+    createProduct(product: string) {
+        // Add an element at the end of the array
+        this._products.push(product);
+
+        // Print the array elements
+        console.log("Products:", this._products)
+        return true;
+    }
 }
